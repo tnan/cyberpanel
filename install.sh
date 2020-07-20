aace75e80e2d9ccef340f8e4aaec4920ccf43384
tnan <mr.tnan@live.com>
2020-07-19 17:03:56 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
+wget https://raw.githubusercontent.com/usmannasir/cyberpanel/v2.0.2-dev/cyberpanel.sh
 printf '1\n1\nY\nn\n2.0.2-dev\nd\nY\nY\nYes\n' | sh cyberpanel.sh 2>&1|tee cyberpanel_install.log

b393dc1907fb8656aba000a081edbf7016cc2c42
tnan <mr.tnan@live.com>
2020-08-18 12:55:23 -0700

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,1 +1,4 @@
-cd ~ && wget https://github.com/tnan/cyberpanel/raw/master/install.sh && chmod +x install.sh && ./install.sh 2>&1|tee cyberpanel_install.log
+cd ~
+wget https://github.com/tnan/cyberpanel/raw/master/install.sh
+chmod +x install.sh
+./install.sh 2>&1|tee cyberpanel_install.log

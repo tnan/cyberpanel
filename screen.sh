6a84c67f2aaf254a8baea06f6d401e0910350a79
tnan <mr.tnan@live.com>
2020-08-18 03:37:29 -0700

    Update screen.sh

--- screen.sh
+++ screen.sh
@@ -1,1 +1,1 @@
-cd ~ && wget https://github.com/tnan/cyberpanel/raw/master/preinstall.sh && chmod +x preinstall.sh && ./preinstall.sh
+cd ~ && yum install -y screen && wget https://github.com/tnan/cyberpanel/raw/master/preinstall.sh && chmod +x preinstall.sh && screen -d -m sh /root/preinstall.sh

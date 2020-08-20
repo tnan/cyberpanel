1953a77df37ed21091102c4171bcb90e998702d8
tnan <mr.tnan@live.com>
2020-08-20 00:07:07 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -77,18 +77,5 @@ sed -i 's#post_max_size = 8M#post_max_size = 10240M#g' /usr/local/lsws/lsphp74/e
 sed -i 's#upload_max_filesize = 2M#upload_max_filesize = 10240M#g' /usr/local/lsws/lsphp74/etc/php.ini
 sed -i 's#allow_url_include = Off#allow_url_include = On#g' /usr/local/lsws/lsphp74/etc/php.ini
 cd ~
-yum install spamassassin -y
-echo "required_score 5" >> /etc/mail/spamassassin/local.cf
-sed -i 's#smtp      inet  n       -       n       -       -       smtpd#smtp      inet  n       -       n       -       -       smtpd -o content_filter=spamassassin#g' /etc/postfix/master.cf
-echo 'spamassassin unix - n n - - pipe flags=R user=spamd argv=/usr/bin/spamc -e /usr/sbin/sendmail -oi -f ${sender} ${recipient}' >> /etc/postfix/master.cf
-groupadd spamd
-useradd -g spamd -s /bin/false -d /var/log/spamassassin spamd
-chown spamd:spamd /var/log/spamassassin
-systemctl enable spamassassin
-systemctl start spamassassin
-systemctl restart postfix
-printf 'yes\n' | sh /root/cyberpanel/CPScripts/mailscannerinstaller.sh
-printf 'yes\nyes\n' | cpan Authen::OATH
-cd ~
 exe_time="$(($SECONDS / 3600)) hrs $((($SECONDS / 60) % 60)) min $(($SECONDS % 60)) sec"
 echo "Done! Execution Time: $exe_time"
